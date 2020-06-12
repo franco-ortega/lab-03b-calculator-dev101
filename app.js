e6d270385291a5fbbc33ca24a004922cf6749171
franco-ortega <francopdx@gmail.com>
2020-06-11 22:26:08 -0700

    additional eslint error corrections

--- app.js
+++ app.js
@@ -3,37 +3,37 @@ import { addNumbers, subtractNumbers, multiplyNumbers, divideNumbers } from './m
 const myInputOne = document.getElementById('inputOne');
 
 myInputOne.addEventListener('input', () => {
-})
+});
 
 const myInputTwo = document.getElementById('inputTwo');
 
 myInputTwo.addEventListener('input', () => {
-})
+});
 
 const myAddButton = document.getElementById('addButton');
 
 myAddButton.addEventListener('click', () => {
-const addAnswer = addNumbers(Number(myInputOne.value), Number(myInputTwo.value));
-calcAnswer.textContent = addAnswer
-})
+    const addAnswer = addNumbers(Number(myInputOne.value), Number(myInputTwo.value));
+    calcAnswer.textContent = addAnswer;
+});
 
 const mySubButton = document.getElementById('subButton');
 
 mySubButton.addEventListener('click', () => {
-const subtractAnswer = subtractNumbers(Number(myInputOne.value), Number(myInputTwo.value));
-calcAnswer.textContent = subtractAnswer
-})
+    const subtractAnswer = subtractNumbers(Number(myInputOne.value), Number(myInputTwo.value));
+    calcAnswer.textContent = subtractAnswer;
+});
 
 const myMulButton = document.getElementById('mulButton');
 
 myMulButton.addEventListener('click', () => {
 const multiplyAnswer = multiplyNumbers(Number(myInputOne.value), Number(myInputTwo.value));
-calcAnswer.textContent = multiplyAnswer
-})
+calcAnswer.textContent = multiplyAnswer;
+});
 
 const myDivButton = document.getElementById('divButton');
 
 myDivButton.addEventListener('click', () => {
 const divideAnswer = divideNumbers(Number(myInputOne.value), Number(myInputTwo.value));
-calcAnswer.textContent = divideAnswer
-})
+calcAnswer.textContent = divideAnswer;
+});

b5d33f98fc0eab635dd69fa5712205569cb10fd6
franco-ortega <francopdx@gmail.com>
2020-06-11 22:20:56 -0700

    correcting eslint errors

--- app.js
+++ app.js
@@ -2,38 +2,38 @@ import { addNumbers, subtractNumbers, multiplyNumbers, divideNumbers } from './m
 
 const myInputOne = document.getElementById('inputOne');
 
-    myInputOne.addEventListener('input', () => {
-    })
+myInputOne.addEventListener('input', () => {
+})
 
-    const myInputTwo = document.getElementById('inputTwo');
+const myInputTwo = document.getElementById('inputTwo');
 
-    myInputTwo.addEventListener('input', () => {
-    })
+myInputTwo.addEventListener('input', () => {
+})
 
 const myAddButton = document.getElementById('addButton');
 
-    myAddButton.addEventListener('click', () => {
-    const addAnswer = addNumbers(Number(myInputOne.value), Number(myInputTwo.value));
-    calcAnswer.textContent = addAnswer
-    })
+myAddButton.addEventListener('click', () => {
+const addAnswer = addNumbers(Number(myInputOne.value), Number(myInputTwo.value));
+calcAnswer.textContent = addAnswer
+})
 
 const mySubButton = document.getElementById('subButton');
 
-    mySubButton.addEventListener('click', () => {
-    const subtractAnswer = subtractNumbers(Number(myInputOne.value), Number(myInputTwo.value));
-    calcAnswer.textContent = subtractAnswer
-    })
+mySubButton.addEventListener('click', () => {
+const subtractAnswer = subtractNumbers(Number(myInputOne.value), Number(myInputTwo.value));
+calcAnswer.textContent = subtractAnswer
+})
 
 const myMulButton = document.getElementById('mulButton');
 
-    myMulButton.addEventListener('click', () => {
-    const multiplyAnswer = multiplyNumbers(Number(myInputOne.value), Number(myInputTwo.value));
-    calcAnswer.textContent = multiplyAnswer
-    })
+myMulButton.addEventListener('click', () => {
+const multiplyAnswer = multiplyNumbers(Number(myInputOne.value), Number(myInputTwo.value));
+calcAnswer.textContent = multiplyAnswer
+})
 
 const myDivButton = document.getElementById('divButton');
 
-    myDivButton.addEventListener('click', () => {
-    const divideAnswer = divideNumbers(Number(myInputOne.value), Number(myInputTwo.value));
-    calcAnswer.textContent = divideAnswer
-    })
+myDivButton.addEventListener('click', () => {
+const divideAnswer = divideNumbers(Number(myInputOne.value), Number(myInputTwo.value));
+calcAnswer.textContent = divideAnswer
+})

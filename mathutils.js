d416352fd0e195aafcd66887812f9809f9bbfa3a
franco-ortega <francopdx@gmail.com>
2020-06-11 22:45:28 -0700

    and yet still even more eslint error corrections

--- mathutils.js
+++ mathutils.js
@@ -1,16 +1,16 @@
 export function addNumbers(numberOne, numberTwo) {
     return numberOne + numberTwo
-}
+};
 
 export function subtractNumbers(numberOne, numberTwo) {
     return numberOne - numberTwo
-}
+};
 
 export function multiplyNumbers(numberOne, numberTwo) {
     return numberOne * numberTwo
-}
+};
 
 export function divideNumbers(numberOne, numberTwo) {
     return numberOne / numberTwo
-}
+};
 
